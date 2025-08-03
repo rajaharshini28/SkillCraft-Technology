0434688d7885d660f0cfc6672ee2801ea903ed80
rajaharshini28 <arepatirajaharshini@gmail.com>
2025-08-03 11:57:51 +0530

    Update script.js

--- SCT_WD_1/script.js
+++ SCT_WD_1/script.js
@@ -1,6 +1,8 @@
-
 window.addEventListener('scroll', () => {
-    const navbar = document.getElementById('navbar');
-    navbar.classList.toggle('scrolled', window.scrollY > 50);
-  });
-  
+  const navbar = document.getElementById('navbar');
+  if (window.scrollY > 50) {
+    navbar.classList.add('scrolled');
+  } else {
+    navbar.classList.remove('scrolled');
+  }
+});
